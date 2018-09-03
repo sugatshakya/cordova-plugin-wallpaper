899bb50f3cea2bb4e5dbb245548167f9a5c83f4b
Sugat <sgt.shakya@gmail.com>
2018-09-03 22:24:33 +0530

    Added set as Intent

--- src/android/wallpaper.java
+++ src/android/wallpaper.java
@@ -12,7 +12,13 @@ import android.graphics.BitmapFactory;
 import android.os.Build;
 import org.apache.cordova.PluginResult;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.File;
+import android.content.Intent;
+import android.net.Uri;
+import android.database.Cursor;
+import android.content.ContentValues;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Images.Media;
 
 public class wallpaper extends CordovaPlugin
 {
@@ -45,24 +51,47 @@ public class wallpaper extends CordovaPlugin
 		try
 		{
 			AssetManager assetManager = context.getAssets();
-			Bitmap bitmap;
 			if(base64) //Base64 encoded
 			{
 				byte[] decoded = android.util.Base64.decode(image, android.util.Base64.DEFAULT);
-				bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+				Bitmap bitmap = BitmapFactory.decodeByteArray(decoded, 0, decoded.length);
+				WallpaperManager myWallpaperManager = WallpaperManager.getInstance(context);
+				myWallpaperManager.setBitmap(bitmap);
 			}
 			else //normal path
 			{
-				InputStream instr = assetManager.open("www/" + image);
-				bitmap = BitmapFactory.decodeStream(instr);
+				image = image.substring(7);
+				File tempFile = new File(image);
+				Uri contentURI = getImageContentUri(cordova.getActivity().getApplicationContext(), tempFile.getAbsolutePath());
+				Intent setAsIntent = new Intent(Intent.ACTION_ATTACH_DATA);
+				setAsIntent.setDataAndType(contentURI, "image/*");
+				setAsIntent.putExtra("mimeType", "image/*");
+				cordova.setActivityResultCallback (this);
+				cordova.getActivity().startActivityForResult(Intent.createChooser(setAsIntent, "Set as"), 1);
 			}
-			WallpaperManager myWallpaperManager = WallpaperManager.getInstance(context);
-			myWallpaperManager.setBitmap(bitmap);
 		}
 		catch (IOException e)
 		{
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+
+	public static Uri getImageContentUri(Context context, String absPath) {
+		Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+				new String[] { MediaStore.Images.Media._ID }, MediaStore.Images.Media.DATA + "=? ",
+				new String[] { absPath }, null);
+
+		if (cursor != null && cursor.moveToFirst()) {
+			int id = cursor.getInt(cursor.getColumnIndex(MediaStore.MediaColumns._ID));
+			return Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, Integer.toString(id));
+
+		} else if (!absPath.isEmpty()) {
+			ContentValues values = new ContentValues();
+			values.put(MediaStore.Images.Media.DATA, absPath);
+			return context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+		} else {
+			return null;
+		}
+	}
+
 }
